0ecb7547f9a31e4e216f780b4746f0fb51fe2691
carnitas <lindsey.cardenas@gmail.com>
2018-12-13 23:29:44 -0600

    added file structure

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE `burgers` (
+
+  -- TABLE CODE TO GO HERE
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    burgerName VARCHAR(255), 
+    devoured BOOLEAN
+);
+
+SELECT * FROM burgers; 
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("HAMBURG", true); 
+
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("CHZBORG", true); 
+
+INSERT INTO burgers (burgerName, devoured)
+VALUES ("BACONBORG", true); 
