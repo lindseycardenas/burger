27c260a90ff406b9d2577baae3e92dbc897687ce
carnitas <lindsey.cardenas@gmail.com>
2018-12-20 18:20:43 -0600

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,17 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection({
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnnection({
         host: "localhost",
         port: 3306,
         user: "root",
         password: "lMc896258!",
         database: "burgers_db"
     });
-};
+}
+
 
 // Make connection.
 connection.connect(function (err) {
